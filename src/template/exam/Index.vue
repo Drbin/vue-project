3dffda12cf976f7723314da761d061ffbca540c2
drbin <dairubin0605@163.com>
2019-11-16 14:47:51 +0800

    测试项目

--- src/template/exam/Index.vue
+++ src/template/exam/Index.vue
@@ -4,7 +4,16 @@
 
 <script>
     export default {
-        name: "Index"
+        name: "Index",
+        mounted () {
+            axios
+            .get('http://202.85.223.75:8090/zpark/ZparkFloorController/queryAllEnergyConsumption')
+                .then(response => (this.info = response))
+                .catch(function (error) { // 请求失败处理
+                    console.log(error);
+                });
+        }
+
     }
 </script>
 
