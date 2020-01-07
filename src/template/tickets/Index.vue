a051f04feea100e829ea1903b8cded23fb708e40
drbin <dairubin0605@163.com>
2020-01-07 17:56:40 +0800

    测试项目

--- src/template/tickets/Index.vue
+++ src/template/tickets/Index.vue
@@ -1,14 +1,33 @@
 <template>
     <div>
         <el-row>
-            <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+            <el-col :span="6">
+                <div class="grid-content">
+                    影片名称：
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content ">
+                    <el-input v-model="input" placeholder="请输入内容"></el-input>
+                </div>
+            </el-col>
+            <el-col :span="4">
+                <div class="grid-content">
+                    <el-button type="primary">搜索</el-button>
+                </div>
+            </el-col>
         </el-row>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Tickets"
+        name: "Tickets",
+        data() {
+            return {
+                input: ''
+            }
+        }
     }
 </script>
 
