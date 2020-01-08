9226b6201c1c95d3667855cbbc281f4bda104085
drbin <dairubin0605@163.com>
2020-01-08 17:34:50 +0800

    测试项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
+import ElementUI,{ Col,Row,Button,Link,Submenu,MenuItem,MenuItemGroup,Menu,Input,Table,TableColumn} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import { Col,Row,Button,Link,Submenu,MenuItem,MenuItemGroup,Menu,Input} from 'element-ui'
 import router from './router'
 
 
@@ -10,6 +9,8 @@ import router from './router'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 Vue.component(Input.name, Input)
+Vue.component(Table.name, Table)
+Vue.component(TableColumn.name, TableColumn)
 Vue.component(Col.name, Col)
 Vue.component(Row.name, Row)
 Vue.component(Button.name, Button)
