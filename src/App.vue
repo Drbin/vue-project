54cd95d447c8112a5feaa6631792db96cf036270
drbin <dairubin0605@163.com>
2020-01-10 17:21:02 +0800

    测试项目

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div class="top">
       <router-link to="/">首页</router-link>
-        <router-link to="/about">个人信息管理</router-link>
-        <router-link to="/about">影片信息查询管理</router-link>
+        <router-link to="/about">个人信息</router-link>
+        <router-link to="/about">影片信息</router-link>
         <router-link to="/tickets">购票信息</router-link>
     </div>
 
