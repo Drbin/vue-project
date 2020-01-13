55d21ce6a20148659c2cb6d617e5dc778cdb5db9
drbin <dairubin0605@163.com>
2020-01-13 17:32:18 +0800

    测试项目

--- src/template/person/Index.vue
+++ src/template/person/Index.vue
@@ -1,10 +1,74 @@
 <template>
-    
+    <div>
+        <el-row>
+            <el-col :span="6">
+                <div class="grid-content">
+                    影片名称：
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content ">
+                    <el-input v-model="input" placeholder="请输入内容"></el-input>
+                </div>
+            </el-col>
+            <el-col :span="4">
+                <div class="grid-content">
+                    <el-button type="primary">搜索</el-button>
+                </div>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24">
+                <el-table
+                        :data="tableData"
+                        border
+                        style="width: 100%">
+                    <el-table-column
+                            prop="date"
+                            label="日期"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            prop="name"
+                            label="姓名"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            prop="address"
+                            label="地址">
+                    </el-table-column>
+                </el-table>
+
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Person"
+        name: "Tickets",
+        data() {
+            return {
+                input: '',
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
+        }
     }
 </script>
 
