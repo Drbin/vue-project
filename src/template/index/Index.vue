a1c62e9d981c7c30cdc6b67ea1db064ecf00965f
drbin <dairubin0605@163.com>
2020-01-20 10:47:50 +0800

    修改 about 页面

--- src/template/index/Index.vue
+++ src/template/index/Index.vue
@@ -2,7 +2,7 @@
 
     <div>
         <el-carousel indicator-position="outside">
-            <el-carousel-item v-for="item in 4" :key="item">
+            <el-carousel-item v-for="item in 5" :key="item">
                 <h3>{{ item }}</h3>
             </el-carousel-item>
         </el-carousel>
