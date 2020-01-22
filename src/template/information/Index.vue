b8c131a374eff3c74fba0a5853b12f4d5addbcd5
drbin <dairubin0605@163.com>
2020-01-22 15:08:03 +0800

    修改 about 页面

--- src/template/information/Index.vue
+++ src/template/information/Index.vue
@@ -1,5 +1,33 @@
 <template>
-    <div></div>
+    <div>
+        <el-row>
+            <el-col :span="3" class="line-height-1">
+                <div class="grid-content">
+                    影片名称：
+                </div>
+            </el-col>
+            <el-col :span="6">
+                <div class="grid-content ">
+                    <el-input v-model="input" placeholder="请输入内容"></el-input>
+                </div>
+            </el-col>
+            <el-col :span="2">
+                <div class="grid-content">
+                    <el-button type="primary">搜索</el-button>
+                </div>
+
+            </el-col>
+            <el-col :span="2">
+
+                <div class="grid-content">
+                    <el-button type="primary">新增</el-button>
+                </div>
+            </el-col>
+        </el-row>
+        <el-row>
+
+        </el-row>
+    </div>
 </template>
 
 <script>
@@ -14,5 +42,7 @@
 </script>
 
 <style scoped>
-
+.shadow-card{
+    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+}
 </style>
