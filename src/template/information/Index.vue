cda3dadd7760eaa5a1da661efb61b3599393f3d6
drbin <dairubin0605@163.com>
2020-01-23 15:09:18 +0800

    修改 about 页面

--- src/template/information/Index.vue
+++ src/template/information/Index.vue
@@ -35,6 +35,30 @@
         name: "Information",
         methods:{
 
+        },
+        data() {
+            return {
+                input: '',
+                tableData: [{
+                    date: '2016-05-02',
+                    code:'ID00011',
+                    name: '王小虎',
+                    power: '1',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    code:'ID00011',
+                    power: '1',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    code:'ID00011',
+                    power: '1',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }]
+            }
         }
 
 
