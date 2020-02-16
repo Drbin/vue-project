c47c26ce1d9ec4ca40fea8a0f21c2444e87f6cff
drbin <dairubin0605@163.com>
2020-02-16 14:21:34 +0800

    测试项目

--- src/template/about/Index.vue
+++ src/template/about/Index.vue
@@ -1,6 +1,15 @@
 <template>
-    <div class="">
+    <div>
+        <div class="top">
+            <router-link to="/" exact>首页</router-link>
+            <router-link to="/Person" >个人信息</router-link>
+            <router-link to="/information" >影片信息</router-link>
+            <router-link to="/tickets" >购票信息</router-link>
+        </div>
 
+        <div class="main">
+            <router-view ></router-view>
+        </div>
     </div>
 
 </template>
