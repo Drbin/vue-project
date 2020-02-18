fd4be34f5dd20cc0d5797c8db698ae82342df778
drbin <dairubin0605@163.com>
2020-02-18 14:24:36 +0800

    测试项目

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div id="app">
-    <div class="top">
-      <router-link to="/" exact>首页</router-link>
-      <router-link to="/Person" >个人信息</router-link>
-      <router-link to="/information" >影片信息</router-link>
-      <router-link to="/tickets" >购票信息</router-link>
-    </div>
-
-    <div class="main">
-      <router-view ></router-view>
-    </div>
+      <router-view></router-view>
   </div>
 </template>
 
@@ -33,21 +24,6 @@ export default {
   color: #2c3e50;
 
 }
-.top{
-  position: fixed;
-  background: #fff;
-  z-index: 99;
-  height: 70px;
-  line-height: 70px;
-  left: 0;
-  top:0;
-  width: 100%;
-  box-shadow: 0 0 12px #ccc;
 
-}
-.top a{ color: #666; margin: 0 10px; font-size:14px ; text-decoration: none;}
-.top a.router-link-exact-active{color: #409eff}
-.top a.router-link-active{color: #409eff;}
 
-.main{ width: 100%; float: left; margin-top: 100px;}
 </style>
