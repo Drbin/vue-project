c42f40469f6886291d24c2eb696bf1cea6bd0e7f
drbin <dairubin0605@163.com>
2020-02-28 23:23:57 +0800

    修改 about 页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/template/index/Index'
-import Information from '@/template/information/Index'
-import Person from '@/template/person/Index'
-import Vote from '@/template/vote/Index'
-import Tickets from '@/template/tickets/Index'
-import Appraise from '@/template/information/appraise'
+import Index from '@/template/front/Index'
+import IndexBase from '@/template/front/index/Index'
+import Back from '@/template/back/Index'
+import Information from '@/template/front/information/Index'
+import Person from '@/template/front/person/Index'
+import Vote from '@/template/front/vote/Index'
+import Tickets from '@/template/front/tickets/Index'
+import Appraise from '@/template/front/information/appraise'
+import PerIndex from '@/template/front/person/Temp'
+import PerAdd from '@/template/front/person/Add'
+import NotFound from '@/template/other/404'
+import Login from '@/template/other/Login'
 
 
 Vue.use(Router)
@@ -15,38 +21,99 @@ export default new Router({
         {
             path:'/',
             name:'Index',
-            component:Index
-        },
-        {
-            path:'/person',
-            name:'Person',
-            component:Person
-
-        },
-        {
-            path:'/tickets',
-            name:'Tickets',
-            component:Tickets
-
-        },
-        {
-            path:'/information',
-            name:'Information',
-            component:Information,
+            component:Index,
             children:[
                 {
-                    path:'/information/appraise',
-                    name:'Appraise',
-                    component:Appraise
+                    path:'/',
+                    name:'IndexBase',
+                    component:IndexBase,
+                },
+                {
+                    path:'/Person/',
+                    name:'Person',
+                    component:Person,
+                    children:[
+                        {
+                            path:'/Person/',
+                            name:'',
+                            component:PerIndex
 
-                }
+                        },
+                        {
+                            path:'/Person/add',
+                            name:'add',
+                            component:PerAdd
+
+                        }
+                    ]
+
+                },
+                {
+                    path:'/Person/',
+                    name:'Person',
+                    component:Person,
+                    children:[
+                        {
+                            path:'/Person/',
+                            name:'',
+                            component:PerIndex
+
+                        },
+                        {
+                            path:'/Person/add',
+                            name:'add',
+                            component:PerAdd
+
+                        }
+                    ]
+
+                },
+                {
+                    path:'/tickets',
+                    name:'Tickets',
+                    component:Tickets
+
+                },
+                {
+                    path:'/information',
+                    name:'Information',
+                    component:Information,
+                    children:[
+                        {
+                            path:'/information/appraise',
+                            name:'Appraise',
+                            component:Appraise
+
+                        }
+                    ]
+
+                },{
+                    path:'/vote',
+                    name:'Vote',
+                    component:Vote
+
+                },
             ]
-
-        },{
-            path:'/vote',
-            name:'Vote',
-            component:Vote
-
+        },
+        {
+            path:'/Back',
+            name:'Back',
+            component:Back
+        },
+        {
+            path: "/Login",
+            name: "Login",
+            component: Login
+        },
+        {
+            path: "/404",
+            name: "NotFound",
+            component: NotFound
+        },
+        {
+            path: "*",
+            redirect: "/404"
         }
+
     ]
 })
