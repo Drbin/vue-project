58ba1b7e3324ca98cfbf5752130ad4cea9ab715a
drbin <dairubin0605@163.com>
2020-03-04 09:28:39 +0800

    测试项目

--- src/template/front/information/info.vue
+++ src/template/front/information/info.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
         <el-row>
-            <el-col :span="3">
-                详情
+            <el-col :span="24">
+                {{tips}}
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24">
+                <h1>{{title}}</h1>
             </el-col>
         </el-row>
     </div>
@@ -10,7 +15,26 @@
 
 <script>
     export default {
-        name: "info"
+        name: "info",
+        data(){
+            return{
+                tips:'No.1',
+                title:'你的名字'
+            }
+        },
+        mounted(){
+          this.getData()
+        },
+        methods: {
+            getData:function () {
+                this.$http.get("http://202.85.223.75:8090/zpark/CyIndexController/queryHomeCompanyData",{
+                    params:{
+                    }
+                }).then(res=>{
+                    window.console.log(res.data)
+                })
+            }
+        }
     }
 </script>
 
