9a3fd700409b32e6fa0cfdb53a71e11571bd45da
drbin <dairubin0605@163.com>
2020-03-10 13:18:41 +0800

    'test'

--- src/template/back/Index.vue
+++ src/template/back/Index.vue
@@ -2,25 +2,37 @@
     <div>
         <el-row class="tac">
             <el-col :span="3">
-                <el-menu default-active="1" class="menu_left">                  
-                    <el-menu-item index="1">
+                <el-menu default-active="1" class="el-menu-vertical-demo menu_left">
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-setting"></i>
+                            <span>配置管理</span>
+                        </template>
+                        <el-menu-item-group>
+                            <el-menu-item index="1-1">&nbsp;权限管理</el-menu-item>
+                            <el-menu-item index="1-2">&nbsp;页面管理</el-menu-item>
+                        </el-menu-item-group>
+                    </el-submenu>
+                    <el-menu-item index="2">
                         <i class="el-icon-menu"></i>
-                        <span slot="title"><router-link to="/Back/Audit">权限管理</router-link></span>
+                        <span slot="title">导航二</span>
                     </el-menu-item>
                     <el-menu-item index="3" >
                         <i class="el-icon-document"></i>
-                        <span slot="title">内容管理</span>
+                        <span slot="title">导航三</span>
                     </el-menu-item>
                     <el-menu-item index="4">
                         <i class="el-icon-setting"></i>
-                        <span slot="title">配置管理</span>
+                        <span slot="title">导航四</span>
                     </el-menu-item>
                 </el-menu>
             </el-col>
             <el-col :span="21">
                 <router-view></router-view>
             </el-col>
+
         </el-row>
+
     </div>
 </template>
 
@@ -31,7 +43,6 @@
     }
 </script>
 
-<style scoped>
-.menu_left a{ text-decoration: none; color: #666; }
-.menu_left .is-active a{color: #409EFF;}
+<style>
+.menu_left .el-menu-item-group__title{display: none;}
 </style>
