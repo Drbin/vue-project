63e22ad694404f92f19a873010157624ab963b4e
drbin <dairubin0605@163.com>
2020-03-12 13:35:06 +0800

    'test'

--- src/template/back/Index.vue
+++ src/template/back/Index.vue
@@ -2,21 +2,26 @@
     <div>
         <el-row class="tac">
             <el-col :span="3">
-                <el-menu default-active="1" class="el-menu-vertical-demo menu_left">
+                <el-menu default-active="2" class="el-menu-vertical-demo menu_left">
+                    <el-menu-item index="2">
+                       
+                        <router-link slot="title" to="/Back/Page">
+                            <i class="el-icon-house"></i>
+                            <span >系统主页</span>
+                        </router-link>
+                      
+                    </el-menu-item>
                     <el-submenu index="1">
                         <template slot="title">
                             <i class="el-icon-setting"></i>
                             <span>配置管理</span>
                         </template>
                         <el-menu-item-group>
-                            <el-menu-item index="1-1">&nbsp;权限管理</el-menu-item>
-                            <el-menu-item index="1-2">&nbsp;页面管理</el-menu-item>
+                            <el-menu-item index="1-1"><router-link to="/Back/Audit">&nbsp;权限管理</router-link></el-menu-item>
+                            <el-menu-item index="1-2"><router-link to="/Back/Page">&nbsp;页面管理</router-link></el-menu-item>
                         </el-menu-item-group>
                     </el-submenu>
-                    <el-menu-item index="2">
-                        <i class="el-icon-menu"></i>
-                        <span slot="title">导航二</span>
-                    </el-menu-item>
+                  
                     <el-menu-item index="3" >
                         <i class="el-icon-document"></i>
                         <span slot="title">导航三</span>
@@ -45,4 +50,6 @@
 
 <style>
 .menu_left .el-menu-item-group__title{display: none;}
+.menu_left a{ display: block; text-decoration: none; color: #666;}
+.menu_left .is-active>a{ color: #409EFF;}
 </style>
