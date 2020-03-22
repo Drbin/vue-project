c7439e2fdb19fd92798dffa1e1338a1c6839b02e
drbin <dairubin0605@163.com>
2020-03-22 17:03:52 +0800

    '后台首页tit'

--- src/template/back/config/page.vue
+++ src/template/back/config/page.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>page</div>
+    <div>
+          <el-row class="b_line">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>首页</el-breadcrumb-item>
+                <el-breadcrumb-item>配置管理</el-breadcrumb-item>
+                <el-breadcrumb-item>页面管理</el-breadcrumb-item>
+            </el-breadcrumb>
+        </el-row>
+    </div>
 </template>
 
 <script>
