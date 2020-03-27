823bebff7ece0c68cabbe1e57ac1780686e6e22d
drbin <dairubin0605@163.com>
2020-03-27 17:36:41 +0800

    '后台首页tit'

--- src/template/back/config/audit.vue
+++ src/template/back/config/audit.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-          <el-row class="b_line">
+        <el-row class="b_line">
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item>首页</el-breadcrumb-item>
                 <el-breadcrumb-item>配置管理</el-breadcrumb-item>
                 <el-breadcrumb-item>权限管理</el-breadcrumb-item>
             </el-breadcrumb>
+        </el-row>
+            <el-row class="query_box">
+            <el-col :span="2" class="query_box_tit">
+                角色名称：
+            </el-col>
+            <el-col :span="6" class="query_box_search">
+                <el-input
+                size="small"
+                placeholder="请输入角色名称"
+                v-model="pageTit"
+                clearable>
+                </el-input>
+            </el-col>
         </el-row>
     </div>
 </template>
