d991f3af8597a374fff929efd84234bf9abcfa0b
drbin <dairubin0605@163.com>
2020-03-29 17:41:56 +0800

    '后台首页tit'

---
+++ src/template/back/config/role.vue
@@ -0,0 +1,124 @@
+<template>
+    <div>
+        <el-row class="b_line">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>首页</el-breadcrumb-item>
+                <el-breadcrumb-item>配置管理</el-breadcrumb-item>
+                <el-breadcrumb-item>角色管理</el-breadcrumb-item>
+            </el-breadcrumb>
+        </el-row>
+        <el-row class="query_box">
+            <el-col :span="2" class="query_box_tit">
+                角色名称：
+            </el-col>
+            <el-col :span="6" class="query_box_search">
+                <el-input
+                size="small"
+                placeholder="请输入角色名称"
+                v-model="auditTit"
+                clearable>
+                </el-input>
+            </el-col>
+        </el-row>
+        <el-row class="pd_50 table_list">
+            <el-table
+                :data="tableData"
+                border
+                style="width: 100%">
+                <el-table-column
+                    prop="date"
+                    label="页面名称"
+                    width="180">
+                </el-table-column>
+                  <el-table-column
+                    prop="name"
+                    label="是否展示"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="address"
+                    label="页面url"
+                   >
+                </el-table-column>
+              
+            </el-table>
+        </el-row>
+        <el-row class="pagi_bot">
+            <div class="pagi_bot_l">
+                共{{total}}条
+            </div>
+            <el-pagination
+                class="pagi_bot_r"
+                page-size='12'
+                background
+                layout="prev, pager, next"
+                :total="total">
+            </el-pagination>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "role",
+        data(){
+            return{
+                auditTit:'',
+                total:100,
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
