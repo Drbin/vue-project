26a95bf138c6ae0a68461daa2fcf03d7bb005311
drbin <dairubin0605@163.com>
2020-04-03 11:01:48 +0800

    用户管理

--- src/template/back/picture/picture.vue
+++ src/template/back/picture/picture.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <el-row>
-            <el-col :span="4" v-for="(o) in 5" :key="o" class="pd_10" >
+            <el-col :span="4" v-for="(o) in 16" :key="o" class="pd_10 height_fixed" >
                 <el-card :body-style="{ padding: '0px' }">
                 <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
                 <div style="padding: 14px;">
@@ -27,6 +27,7 @@
     }
 </script>
 
-<style scoped>
+<style>
+    .height_fixed img{ width: 100%; height: 265px; }
 
 </style>
