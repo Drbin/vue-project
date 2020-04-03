492cb789991f481c62d9b7278b0a3c6328699147
drbin <dairubin0605@163.com>
2020-04-03 10:59:36 +0800

    用户管理

--- src/template/back/picture/picture.vue
+++ src/template/back/picture/picture.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <el-row>
-            <el-col :span="8" v-for="(o, index) in 5" :key="o" :offset="index > 0 ? 2 : 0">
+            <el-col :span="4" v-for="(o) in 5" :key="o" class="pd_10" >
                 <el-card :body-style="{ padding: '0px' }">
                 <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
                 <div style="padding: 14px;">
