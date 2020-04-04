1d6a2fc856175acc361da933d7b2289726a22156
drbin <dairubin0605@163.com>
2020-04-04 11:17:56 +0800

    图片管理

--- src/template/back/picture/picture.vue
+++ src/template/back/picture/picture.vue
@@ -8,7 +8,7 @@
             </el-breadcrumb>
         </el-row>
         <el-row>
-            <el-col :span="4" v-for="(o) in 12" :key="o" class="pd_10 height_fixed" >
+            <el-col :span="4" v-for="i in 12" :key="i" class="pd_10 height_fixed" >
                 <el-card :body-style="{ padding: '0px' }">
                 <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
                 <div style="padding: 14px;">
@@ -27,7 +27,7 @@
             </div>
             <el-pagination
                 class="pagi_bot_r"
-                page-size=12
+                :page-size="12"
                 background
                 layout="prev, pager, next"
                 :total="total">
