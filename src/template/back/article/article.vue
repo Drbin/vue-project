06cf088b2736a9368d891d702f160a49bd9b1f5b
drbin <dairubin0605@163.com>
2020-04-05 11:29:09 +0800

    图片管理

--- src/template/back/article/article.vue
+++ src/template/back/article/article.vue
@@ -1,8 +1,12 @@
 <template>
     <div>
-        
+        <el-row class="b_line">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>首页</el-breadcrumb-item>
+                <el-breadcrumb-item>文章管理</el-breadcrumb-item>
+            </el-breadcrumb>
+        </el-row>
     </div>
-
 </template>
 
 <script>
