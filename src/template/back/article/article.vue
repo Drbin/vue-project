d69e325ab6b3648fe37096958594c39a97649ce2
drbin <dairubin0605@163.com>
2020-04-05 11:30:59 +0800

    用户管理

--- src/template/back/article/article.vue
+++ src/template/back/article/article.vue
@@ -6,12 +6,42 @@
                 <el-breadcrumb-item>文章管理</el-breadcrumb-item>
             </el-breadcrumb>
         </el-row>
+        <el-row class="query_box">
+            <el-col :span="2" class="query_box_tit">
+                文章名称：
+            </el-col>
+            <el-col :span="6" class="query_box_search">
+                <el-input
+                size="small"
+                placeholder="请输入角色名称"
+                v-model="auditTit"
+                clearable>
+                </el-input>
+            </el-col>
+        </el-row>
+        <el-row class="pagi_bot">
+            <div class="pagi_bot_l">
+                共{{total}}条
+            </div>
+            <el-pagination
+                class="pagi_bot_r"
+                :page-size="12"
+                background
+                layout="prev, pager, next"
+                :total="total">
+            </el-pagination>
+        </el-row>
     </div>
 </template>
 
 <script>
     export default {
         name: "Article",
+        data(){
+            return {
+                total:100,
+            }
+        },
         methods: {
 
         }
