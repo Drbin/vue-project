d64b2a0ea05cb79258ac25314b08597c4fd71400
drbin <dairubin0605@163.com>
2020-04-15 17:37:36 +0800

    图片上传

--- src/template/back/article/article.vue
+++ src/template/back/article/article.vue
@@ -31,9 +31,30 @@
                 </router-link>
             </el-col>
         </el-row>
-        <el-row class="pd_50 table_list">
-
+             <el-row class="pd_50 table_list">
+            <el-table
+                :data="tableData"
+                border
+                style="width: 100%">
+                <el-table-column
+                    prop="date"
+                    label="页面名称"
+                    width="180">
+                </el-table-column>
+                  <el-table-column
+                    prop="name"
+                    label="是否展示"
+                    width="180">
+                </el-table-column>
+                <el-table-column
+                    prop="address"
+                    label="页面url"
+                   >
+                </el-table-column>
+              
+            </el-table>
         </el-row>
+
         <el-row class="pagi_bot">
             <div class="pagi_bot_l">
                 共{{total}}条
