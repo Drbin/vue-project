d6d08e8afda67d60532d7919d40e98aca66656b0
代汝斌 <dairubin0605@163.com>
2020-04-18 14:27:51 +0800

    '详情'

--- src/template/front/information/msg.vue
+++ src/template/front/information/msg.vue
@@ -4,6 +4,7 @@
         <div class="content_txt">
             sad sdad asds asd as asd sdd dd s 
         </div>
+        <div class="content_author"></div>
         <div class="content_time">2018-04-13</div>
     </div>
 
@@ -15,17 +16,7 @@
         name:'msg',
         data() {
             return {
-                reverse: true,
-                activities: [{
-                    content: '活动按期开始',
-                    timestamp: '2018-04-15'
-                }, {
-                    content: '通过审核',
-                    timestamp: '2018-04-13'
-                }, {
-                    content: '创建成功',
-                    timestamp: '2018-04-11'
-                }]
+              
             };
         }
     };
@@ -33,7 +24,7 @@
 <style scoped>
 .content_tit{ padding-bottom: 10px; border-bottom: 1px solid #ccc; line-height: 20px; font-size: 18px; width: 100%; text-align: center;}
 .content_txt{ font-size: 12px; width: 100%; margin-top: 10px;  color: #666; border-bottom: 1px solid #ccc; }
-.content_time{ width: 100%; height: 20px; text-align: right;;}
+.content_time{ width: 50%; float: left; height: 20px; text-align: right;;}
 
 
 </style>
