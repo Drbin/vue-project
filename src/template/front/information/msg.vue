7483a9123a1c88c5dd2d54f3906cbdd861e56bea
代汝斌 <dairubin0605@163.com>
2020-04-18 14:29:52 +0800

    '详情'

--- src/template/front/information/msg.vue
+++ src/template/front/information/msg.vue
@@ -4,7 +4,7 @@
         <div class="content_txt">
             sad sdad asds asd as asd sdd dd s 
         </div>
-        <div class="content_author"></div>
+        <div class="content_author">{{author}}</div>
         <div class="content_time">2018-04-13</div>
     </div>
 
@@ -16,6 +16,7 @@
         name:'msg',
         data() {
             return {
+                author:'admin'
               
             };
         }
@@ -25,6 +26,6 @@
 .content_tit{ padding-bottom: 10px; border-bottom: 1px solid #ccc; line-height: 20px; font-size: 18px; width: 100%; text-align: center;}
 .content_txt{ font-size: 12px; width: 100%; margin-top: 10px;  color: #666; border-bottom: 1px solid #ccc; }
 .content_time{ width: 50%; float: left; height: 20px; text-align: right;;}
-
+.content_author{  width: 50%; float: left; height: 20px;}
 
 </style>
