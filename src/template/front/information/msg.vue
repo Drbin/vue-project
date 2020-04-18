576b608719d5cc80adce13a203a93dc86cc9a949
代汝斌 <dairubin0605@163.com>
2020-04-18 14:23:47 +0800

    '详情'

--- src/template/front/information/msg.vue
+++ src/template/front/information/msg.vue
@@ -4,7 +4,7 @@
         <div class="content_txt">
             sad sdad asds asd as asd sdd dd s 
         </div>
-        <div class="content_time"
+        <div class="content_time">2018-04-13</div>
     </div>
 
 </template>
@@ -31,5 +31,9 @@
     };
 </script>
 <style scoped>
+.content_tit{ padding-bottom: 10px; border-bottom: 1px solid #ccc; line-height: 20px; font-size: 18px; width: 100%; text-align: center;}
+.content_txt{ font-size: 12px; width: 100%; margin-top: 10px;  color: #666; border-bottom: 1px solid #ccc; }
+.content_time{ width: 100%; height: 20px; text-align: right;;}
+
 
 </style>
